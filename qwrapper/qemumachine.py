afa6685a4f0d8a34bf1db97c9b041ccff0026b83
Daniel Dybing <danieldy@uia.no>
2024-03-06 19:20:43 +0000

    Removed functionality which is unnecessary at the time being

--- qwrapper/qemumachine.py
+++ qwrapper/qemumachine.py
@@ -1,9 +1,4 @@
 
-
-
-from qemu.qmp import QMPClient
-import pygdbmi
-
 from abc import ABC, abstractmethod
 
 
@@ -24,25 +19,5 @@ class QemuMachine(ABC):
         # This method cleans up the QEMU instance and frees up the resources. 
         pass
 
-    def stop():
-        # This method stops the QEMU instance, without deleting it or freeing up resources.
-        pass
-
-    @abstractmethod
-    def get_single_register(self, register):
-        # This method gets the value of a single register from the QEMU instance.
-        pass
-
-    @abstractmethod
-    def get_all_registers(self):
-        # This method gets the value of all registers from the QEMU instance.
-        pass
-
 class X86Machine:
-    pass
-
-class PPCMachine:
-    pass
-
-class M68KMachine:
     pass
