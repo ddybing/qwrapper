b613d67a624bf07e8c9525702faa6793f4d6e794
Daniel Dybing <danieldy@uia.no>
2024-03-17 15:11:16 +0100

    Imported missing dependency and now waits after initialisation

--- qwrapper/qemumachine.py
+++ qwrapper/qemumachine.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
 import asyncio
+import atexit
+import time
 
 from qemu.qmp import QMPClient
 
@@ -33,12 +35,15 @@ class QemuMachine(ABC):
 class X86Machine(QemuMachine):
         def __init__(self):
             super().__init__()
-            qemuCmd = ['qemu-system-i386', '-display', 'none', '-s', '-qmp', 'unix:/tmp/qmp.sock,server=on,wait=off', '-hda', 'kernel.iso', '-hdb', 'disk.iso']
+            qemuCmd = ['qemu-system-i386',  '-s', '-qmp', 'unix:/tmp/qmp.socket,server=on,wait=on', '-hda', 'kernel.iso', '-hdb', 'disk.iso']
             print("Setting up virtual machine...")
             # Start Qemu with gdb stub and QMP Server. Add the kernel and disk images.
             # Virtual machine starts in "wait" mode. Waits for user to start VM execution.
             self.QemuProcess = subprocess.Popen(qemuCmd)
             atexit.register(self.cleanup)
+
+            # Wait 2 seconds to allow QEMU to create the socket file
+            time.sleep(2) 
         
         def cleanup(self):
             print("Shutting down QEMU machine...")
