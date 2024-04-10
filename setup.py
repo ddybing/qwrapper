831b3504811795ab43467a0ebbeff2d06f48d97b
Daniel Dybing <danieldy@uia.no>
2024-04-10 20:54:24 +0200

    Updated QWrapper version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qwrapper',
-    version='0.1.11',
+    version='0.1.12',
     description='A QEMU wrapper for Python',
     author='Daniel Dybing',
     author_email='danieldy@uia.no',
