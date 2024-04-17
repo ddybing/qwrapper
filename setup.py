70511ac2c75566ac92a5f0fd7a1ca4d5a91dfb36
Daniel Dybing <danieldy@uia.no>
2024-04-17 13:10:32 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qwrapper',
+    version='0.1.13',
+    description='A QEMU wrapper for Python',
+    author='Daniel Dybing',
+    author_email='danieldy@uia.no',
+    packages=find_packages(),
+    install_requires=[
+        'pygdbmi',
+        'qemu.qmp',
+        # List your project dependencies here
+        # For example:
+        # 'numpy>=1.18.1',
+        # 'pandas>=1.0.1',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
