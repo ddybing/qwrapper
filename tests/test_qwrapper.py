c22b06894401dcc7e2821f4c77aca103060c00fe
Daniel Dybing <danieldy@uia.no>
2024-04-24 19:31:58 +0200

    Added test for memory

--- tests/test_qwrapper.py
+++ tests/test_qwrapper.py
@@ -1,5 +1,12 @@
 from qwrapper.x86 import X86Machine
 
-# Try to create a machine
+# Create a machine
 def test_x86machine():
     machine = X86Machine()
+
+
+# Create machine and read memory
+def test_x86_machine():
+    machine = X86Machine()
+    memory = machine.read_memory_bytes(0x0, 100)
+    assert memory is not None
