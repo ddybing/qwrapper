40734eded1b1eec63e1ded9587f41b4d309549f2
Daniel Dybing <danieldy@uia.no>
2024-04-24 21:13:06 +0200

    Rewritten last test

--- tests/test_qwrapper_x86.py
+++ tests/test_qwrapper_x86.py
@@ -18,18 +18,21 @@ def test_x86_machine_memory():
     assert memory is not None
 
 # Create machine and add disk files
-# def test_x86machine_diskimages():
-#     machine = X86Machine(['tests/disk.iso', 'tests/disk2.iso'], False)
+def test_x86machine_diskimages():
+    machine = X86Machine(['tests/disk.iso', 'tests/disk2.iso'], False)
 
 
-#     # Check if the machine boots the disk images.
-#     # Should say 'Hello World' on screen after 10 seconds.
-#     # VGA_MEMORY = [
-#     # '0x48', '0x07', '0x65', '0x07', '0x6c', '0x07', '0x6c', '0x07',
-#     # '0x6f', '0x07', '0x20', '0x07', '0x57', '0x07', '0x6f', '0x07',
-#     # '0x72', '0x07', '0x6c', '0x07', '0x64', '0x07']
+     # Check if the machine boots the disk images.
+     # Should say 'Hello World' on screen after 10 seconds.
+    VGA_MEMORY = [
+     '0x48', '0x07', '0x65', '0x07', '0x6c', '0x07', '0x6c', '0x07',
+      '0x6f', '0x07', '0x20', '0x07', '0x57', '0x07', '0x6f', '0x07',
+     '0x72', '0x07', '0x6c', '0x07', '0x64', '0x07']
 
-#     # machine.start()
-#     # screen_memory = machine.read_memory_bytes(0xB8000, 22)
+    machine.start()
+    time.sleep(10)
+    screen_memory = machine.read_memory_bytes(0xB8000, 22)
+    machine.stop()
+    machine.cleanup()
 
-#     # assert screen_memory == VGA_MEMORY
+    assert screen_memory == VGA_MEMORY
